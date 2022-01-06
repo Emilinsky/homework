db04df9eaa97f325021f789d4592aeef37eadffe
Emilinsky <emilinsky260@gmail.com>
2022-01-06 19:20:27 +0200

    first commit

---
+++ index.js
@@ -0,0 +1,32 @@
+const num1 = document.querySelectorAll("#aCipars")
+const num2 = document.querySelectorAll("#bCipars")
+const plusBtn
+function plus(a,b){
+    return a+b;
+}
+
+function minus(a,b){
+    return a-b;
+}
+
+function reiz(a,b){
+    return a*b;
+}
+
+function dalit(a,b){
+    return a/b;
+}
+
+function checkNumber{
+if isNaN(aCipars) = true
+alert('A vertiba nav cipars')
+else
+a=aCipars;
+
+
+
+if isNaN(bCipars) = true
+alert('B vertiba nav cipars')
+else
+b=bCipars;
+}
